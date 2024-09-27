a86e5dca0f66576b69cf2797b6b233c0bf212802
Eric Wollesen <eric@tidepool.org>
2024-09-27 09:57:26 -0600

    weirdness with ginkgo.BeforeSuite

--- store/structured/mongo/mongo_suite_test.go
+++ store/structured/mongo/mongo_suite_test.go
@@ -1,7 +1,10 @@
 package mongo_test
 
 import (
+	"fmt"
 	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
 
 	"github.com/tidepool-org/platform/test"
 )
@@ -9,3 +12,7 @@ import (
 func TestSuite(t *testing.T) {
 	test.Test(t)
 }
+
+var _ = BeforeSuite(func() {
+	fmt.Println("before suite store/mongo")
+})
