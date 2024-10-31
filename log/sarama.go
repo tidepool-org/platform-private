tidepool-org/platform

adapts sarama.Logger to implement log.Logger
So that sarama log messages better follow our standards, and will be emitted
as JSON when log.Logger is configured for that.

Before this change, the sarama logs were printed in plain-text without any of
the benefits of the platform log.Logger.

BACK-2554

log/sarama.go
@@ -1,0 +1,35 @@
+package log
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Shopify/sarama"
+)
+
+// NewSarama returns a [Logger] adapted to implement [sarama.StdLogger].
+func NewSarama(l Logger) sarama.StdLogger {
+	return &SaramaLogger{Logger: l.WithField("SARAMA", "1")}
+}
+
+// SaramaLogger wraps a [Logger] to implement [sarama.StdLogger].
+//
+// Sarama doesn't support the concept of logging levels, so all messages will
+// use the info level.
+type SaramaLogger struct {
+	Logger
+}
+
+func (l *SaramaLogger) Print(args ...interface{}) {
+	l.Logger.Info(fmt.Sprint(args...))
+}
+
+func (l *SaramaLogger) Printf(format string, args ...interface{}) {
+	// Sarama log messages sent via this method include a newline, which
+	// doesn't fit with Logger's style, so remove it.
+	l.Logger.Infof(strings.TrimSuffix(format, "\n"), args...)
+}
+
+func (l *SaramaLogger) Println(args ...interface{}) {
+	l.Logger.Info(fmt.Sprint(args...))
+}

tidepool-org/platform-private

log/sarama.go
@@ -1,0 +1,35 @@
+package log
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/IBM/sarama"
+)
+
+// NewSarama returns a [Logger] adapted to implement [sarama.StdLogger].
+func NewSarama(l Logger) sarama.StdLogger {
+	return &SaramaLogger{Logger: l.WithField("SARAMA", "1")}
+}
+
+// SaramaLogger wraps a [Logger] to implement [sarama.StdLogger].
+//
+// Sarama doesn't support the concept of logging levels, so all messages will
+// use the info level.
+type SaramaLogger struct {
+	Logger
+}
+
+func (l *SaramaLogger) Print(args ...interface{}) {
+	l.Logger.Info(fmt.Sprint(args...))
+}
+
+func (l *SaramaLogger) Printf(format string, args ...interface{}) {
+	// Sarama log messages sent via this method include a newline, which
+	// doesn't fit with Logger's style, so remove it.
+	l.Logger.Infof(strings.TrimSuffix(format, "\n"), args...)
+}
+
+func (l *SaramaLogger) Println(args ...interface{}) {
+	l.Logger.Info(fmt.Sprint(args...))
+}