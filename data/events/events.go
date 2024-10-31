tidepool-org/platform

adapts go-common's asyncevents.SaramaEventsConsumer for alerts
The existing FaultTolerantConsumer isn't used because it's retry semantics are
hard-wired and aren't compatible with what care partner alerting's needs.

Note: A proper implementation of AlertsEventsConsumer to consume events is yet
to be written. It will follow shortly.

BACK-2554

data/events/events.go
@@ -2,7 +2,14 @@ package events
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+	"sync"
+	"time"
 
+	"github.com/Shopify/sarama"
+
+	"github.com/tidepool-org/go-common/asyncevents"
 	ev "github.com/tidepool-org/go-common/events"
 
 	dataSourceStoreStructured "github.com/tidepool-org/platform/data/source/store/structured"
@@ -55,6 +62,138 @@ func (u *userDeletionEventsHandler) HandleDeleteUserEvent(payload ev.DeleteUserE
 
 	if len(errs) != 0 {
 		return errors.New("Unable to delete device data for user")
+	}
+	return nil
+}
+
+// AlertsEventRetryDelayMaximum is the maximum delay between consumption
+// retries.
+const AlertsEventRetryDelayMaximum = time.Minute
+
+// AlertsEventRetries is the maximum consumption attempts before giving up.
+const AlertsEventRetries = 1000
+
+// AlertsEventConsumptionTimeout is the maximum time to process an alerts event.
+const AlertsEventConsumptionTimeout = 30 * time.Second
+
+// SaramaRunner interfaces between events.Runner and go-common's
+// asyncevents.SaramaEventsConsumer.
+type SaramaRunner struct {
+	EventsRunner SaramaEventsRunner
+	Config       SaramaRunnerConfig
+	cancelCtx    context.CancelFunc
+	cancelMu     sync.Mutex
+}
+
+// SaramaEventsRunner is implemented by go-common's
+// asyncevents.SaramaEventsRunner.
+type SaramaEventsRunner interface {
+	Run(ctx context.Context) error
+}
+
+// SaramaRunnerConfig collects values needed to initialize a SaramaRunner.
+//
+// This provides isolation for the SaramaRunner from ConfigReporter,
+// envconfig, or any of the other options in platform for reading config
+// values.
+type SaramaRunnerConfig struct {
+	Brokers         []string
+	GroupID         string
+	Logger          log.Logger
+	Topics          []string
+	MessageConsumer asyncevents.SaramaMessageConsumer
+
+	Sarama *sarama.Config
+}
+
+func (r *SaramaRunner) Initialize() error {
+	group, err := sarama.NewConsumerGroup(r.Config.Brokers, r.Config.GroupID, r.Config.Sarama)
+	if err != nil {
+		return errors.Wrap(err, "Unable to build sarama consumer group")
+	}
+	handler := asyncevents.NewSaramaConsumerGroupHandler(&asyncevents.NTimesRetryingConsumer{
+		Consumer: r.Config.MessageConsumer,
+		Delay:    CappedExponentialBinaryDelay(AlertsEventRetryDelayMaximum),
+		Times:    AlertsEventRetries,
+		Logger:   r.logger,
+	}, AlertsEventConsumptionTimeout)
+	r.EventsRunner = asyncevents.NewSaramaEventsConsumer(group, handler, r.Config.Topics...)
+	return nil
+}
+
+func (r *SaramaRunner) logger(ctx context.Context) asyncevents.Logger {
+	// Prefer a logger from the context.
+	if ctxLogger := log.LoggerFromContext(ctx); ctxLogger != nil {
+		return &log.GoCommonAdapter{Logger: ctxLogger}
+	}
+	if r.Config.Logger != nil {
+		return &log.GoCommonAdapter{Logger: r.Config.Logger}
+	}
+	// No known log.Logger could be found, default to slog.
+	return slog.Default()
+}
+
+// Run adapts platform's event.Runner to work with go-common's
+// asyncevents.SaramaEventsConsumer.
+func (r *SaramaRunner) Run() error {
+	if r.EventsRunner == nil {
+		return errors.New("Unable to run SaramaRunner, EventsRunner is nil")
+	}
+
+	r.cancelMu.Lock()
+	ctx, err := func() (context.Context, error) {
+		defer r.cancelMu.Unlock()
+		if r.cancelCtx != nil {
+			return nil, errors.New("Unable to Run SaramaRunner, it's already initialized")
+		}
+		var ctx context.Context
+		ctx, r.cancelCtx = context.WithCancel(context.Background())
+		return ctx, nil
+	}()
+	if err != nil {
+		return err
+	}
+	if err := r.EventsRunner.Run(ctx); err != nil {
+		return errors.Wrap(err, "Unable to Run SaramaRunner")
+	}
+	return nil
+}
+
+// Terminate adapts platform's event.Runner to work with go-common's
+// asyncevents.SaramaEventsConsumer.
+func (r *SaramaRunner) Terminate() error {
+	r.cancelMu.Lock()
+	defer r.cancelMu.Unlock()
+	if r.cancelCtx == nil {
+		return errors.New("Unable to Terminate SaramaRunner, it's not running")
+	}
+	r.cancelCtx()
+	return nil
+}
+
+// CappedExponentialBinaryDelay builds delay functions that use exponential
+// binary backoff with a maximum duration.
+func CappedExponentialBinaryDelay(cap time.Duration) func(int) time.Duration {
+	return func(tries int) time.Duration {
+		b := asyncevents.DelayExponentialBinary(tries)
+		if b > cap {
+			return cap
+		}
+		return b
+	}
+}
+
+// TODO: implement me!!
+type AlertsEventsConsumer struct {
+	Consumer asyncevents.SaramaMessageConsumer
+}
+
+func (c *AlertsEventsConsumer) Consume(ctx context.Context,
+	session sarama.ConsumerGroupSession, message *sarama.ConsumerMessage) error {
+	err := c.Consumer.Consume(ctx, session, message)
+	if err != nil {
+		session.MarkMessage(message, fmt.Sprintf("I have given up after error: %s", err))
+		return err
 	}
 	return nil
 }

data/events/events_test.go
@@ -1,0 +1,138 @@
+package events
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/tidepool-org/platform/log"
+	"github.com/tidepool-org/platform/log/test"
+)
+
+var _ = Describe("SaramaRunner", func() {
+	Context("has a lifecycle", func() {
+		newTestRunner := func() *SaramaRunner {
+			return &SaramaRunner{
+				Config:       SaramaRunnerConfig{},
+				EventsRunner: &mockEventsRunner{},
+			}
+		}
+		It("starts with Run() and stops with Terminate()", func() {
+			r := newTestRunner()
+			var termErr error
+			go func() {
+				time.Sleep(time.Millisecond)
+				termErr = r.Terminate()
+			}()
+			Expect(r.Run()).To(Succeed())
+			Eventually(termErr).WithTimeout(10 * time.Millisecond).Should(Succeed())
+		})
+
+		Describe("Run()", func() {
+			It("can be started only once", func() {
+				r := newTestRunner()
+				var firstRunErr, secondRunErr error
+				launched := make(chan struct{}, 2)
+				go func() {
+					launched <- struct{}{}
+					firstRunErr = r.Run()
+				}()
+				go func() {
+					launched <- struct{}{}
+					secondRunErr = r.Run()
+				}()
+				<-launched
+				<-launched
+
+				// The above doesn't _guarantee_ that Run has been called twice,
+				// but... it should work.
+
+				Expect(r.Terminate()).To(Succeed())
+				if firstRunErr != nil {
+					Expect(firstRunErr).To(MatchError(ContainSubstring("it's already initialized")))
+					Expect(secondRunErr).To(Succeed())
+				} else {
+					Expect(firstRunErr).To(Succeed())
+					Expect(secondRunErr).To(MatchError(ContainSubstring("it's already initialized")))
+				}
+			})
+
+			It("can't be Terminate()'d before it's Run()", func() {
+				r := newTestRunner()
+				Expect(r.Terminate()).To(MatchError(ContainSubstring("it's not running")))
+			})
+		})
+	})
+
+	Describe("logger", func() {
+		It("prefers a context's logger", func() {
+			testLogger := test.NewLogger()
+			ctxLogger := test.NewLogger()
+			r := &SaramaRunner{
+				Config: SaramaRunnerConfig{Logger: testLogger},
+			}
+
+			ctx := log.NewContextWithLogger(context.Background(), ctxLogger)
+			got := r.logger(ctx)
+
+			goCommonLogger, ok := got.(*log.GoCommonAdapter)
+			Expect(ok).To(BeTrue())
+			Expect(goCommonLogger.Logger).To(Equal(ctxLogger))
+		})
+
+		Context("without a context logger", func() {
+			It("uses the configured logger", func() {
+				testLogger := test.NewLogger()
+				r := &SaramaRunner{
+					Config: SaramaRunnerConfig{
+						Logger: testLogger,
+					},
+				}
+
+				got := r.logger(context.Background())
+
+				goCommonLogger, ok := got.(*log.GoCommonAdapter)
+				Expect(ok).To(BeTrue())
+				Expect(goCommonLogger.Logger).To(Equal(testLogger))
+			})
+
+			Context("or any configured logger", func() {
+				It("doesn't panic", func() {
+					r := &SaramaRunner{Config: SaramaRunnerConfig{}}
+					ctx := context.Background()
+					got := r.logger(ctx)
+
+					Expect(func() {
+						got.Log(ctx, slog.LevelInfo, "testing")
+					}).ToNot(Panic())
+				})
+			})
+		})
+	})
+
+	DescribeTable("CappedExponentialBinaryDelay",
+		func(cap time.Duration, input int, output time.Duration) {
+			f := CappedExponentialBinaryDelay(cap)
+			Expect(f(input)).To(Equal(output))
+		},
+		Entry("cap: 1m; tries: 0", time.Minute, 0, time.Second),
+		Entry("cap: 1m; tries: 1", time.Minute, 1, 2*time.Second),
+		Entry("cap: 1m; tries: 2", time.Minute, 2, 4*time.Second),
+		Entry("cap: 1m; tries: 3", time.Minute, 3, 8*time.Second),
+		Entry("cap: 1m; tries: 4", time.Minute, 4, 16*time.Second),
+		Entry("cap: 1m; tries: 5", time.Minute, 5, 32*time.Second),
+		Entry("cap: 1m; tries: 6", time.Minute, 6, time.Minute),
+		Entry("cap: 1m; tries: 20", time.Minute, 20, time.Minute),
+	)
+})
+
+type mockEventsRunner struct {
+	Err error
+}
+
+func (r *mockEventsRunner) Run(ctx context.Context) error {
+	return r.Err
+}

tidepool-org/platform-private

data/events/events.go
@@ -2,7 +2,14 @@ package events
 
 import (
 	"context"
+	"fmt"
+	"log/slog"
+	"sync"
+	"time"
 
+	"github.com/IBM/sarama"
+
+	"github.com/tidepool-org/go-common/asyncevents"
 	ev "github.com/tidepool-org/go-common/events"
 
 	dataSourceStoreStructured "github.com/tidepool-org/platform/data/source/store/structured"
@@ -55,6 +62,138 @@ func (u *userDeletionEventsHandler) HandleDeleteUserEvent(payload ev.DeleteUserE
 
 	if len(errs) != 0 {
 		return errors.New("Unable to delete device data for user")
+	}
+	return nil
+}
+
+// AlertsEventRetryDelayMaximum is the maximum delay between consumption
+// retries.
+const AlertsEventRetryDelayMaximum = time.Minute
+
+// AlertsEventRetries is the maximum consumption attempts before giving up.
+const AlertsEventRetries = 1000
+
+// AlertsEventConsumptionTimeout is the maximum time to process an alerts event.
+const AlertsEventConsumptionTimeout = 30 * time.Second
+
+// SaramaRunner interfaces between events.Runner and go-common's
+// asyncevents.SaramaEventsConsumer.
+type SaramaRunner struct {
+	EventsRunner SaramaEventsRunner
+	Config       SaramaRunnerConfig
+	cancelCtx    context.CancelFunc
+	cancelMu     sync.Mutex
+}
+
+// SaramaEventsRunner is implemented by go-common's
+// asyncevents.SaramaEventsRunner.
+type SaramaEventsRunner interface {
+	Run(ctx context.Context) error
+}
+
+// SaramaRunnerConfig collects values needed to initialize a SaramaRunner.
+//
+// This provides isolation for the SaramaRunner from ConfigReporter,
+// envconfig, or any of the other options in platform for reading config
+// values.
+type SaramaRunnerConfig struct {
+	Brokers         []string
+	GroupID         string
+	Logger          log.Logger
+	Topics          []string
+	MessageConsumer asyncevents.SaramaMessageConsumer
+
+	Sarama *sarama.Config
+}
+
+func (r *SaramaRunner) Initialize() error {
+	group, err := sarama.NewConsumerGroup(r.Config.Brokers, r.Config.GroupID, r.Config.Sarama)
+	if err != nil {
+		return errors.Wrap(err, "Unable to build sarama consumer group")
+	}
+	handler := asyncevents.NewSaramaConsumerGroupHandler(&asyncevents.NTimesRetryingConsumer{
+		Consumer: r.Config.MessageConsumer,
+		Delay:    CappedExponentialBinaryDelay(AlertsEventRetryDelayMaximum),
+		Times:    AlertsEventRetries,
+		Logger:   r.logger,
+	}, AlertsEventConsumptionTimeout)
+	r.EventsRunner = asyncevents.NewSaramaEventsConsumer(group, handler, r.Config.Topics...)
+	return nil
+}
+
+func (r *SaramaRunner) logger(ctx context.Context) asyncevents.Logger {
+	// Prefer a logger from the context.
+	if ctxLogger := log.LoggerFromContext(ctx); ctxLogger != nil {
+		return &log.GoCommonAdapter{Logger: ctxLogger}
+	}
+	if r.Config.Logger != nil {
+		return &log.GoCommonAdapter{Logger: r.Config.Logger}
+	}
+	// No known log.Logger could be found, default to slog.
+	return slog.Default()
+}
+
+// Run adapts platform's event.Runner to work with go-common's
+// asyncevents.SaramaEventsConsumer.
+func (r *SaramaRunner) Run() error {
+	if r.EventsRunner == nil {
+		return errors.New("Unable to run SaramaRunner, EventsRunner is nil")
+	}
+
+	r.cancelMu.Lock()
+	ctx, err := func() (context.Context, error) {
+		defer r.cancelMu.Unlock()
+		if r.cancelCtx != nil {
+			return nil, errors.New("Unable to Run SaramaRunner, it's already initialized")
+		}
+		var ctx context.Context
+		ctx, r.cancelCtx = context.WithCancel(context.Background())
+		return ctx, nil
+	}()
+	if err != nil {
+		return err
+	}
+	if err := r.EventsRunner.Run(ctx); err != nil {
+		return errors.Wrap(err, "Unable to Run SaramaRunner")
+	}
+	return nil
+}
+
+// Terminate adapts platform's event.Runner to work with go-common's
+// asyncevents.SaramaEventsConsumer.
+func (r *SaramaRunner) Terminate() error {
+	r.cancelMu.Lock()
+	defer r.cancelMu.Unlock()
+	if r.cancelCtx == nil {
+		return errors.New("Unable to Terminate SaramaRunner, it's not running")
+	}
+	r.cancelCtx()
+	return nil
+}
+
+// CappedExponentialBinaryDelay builds delay functions that use exponential
+// binary backoff with a maximum duration.
+func CappedExponentialBinaryDelay(cap time.Duration) func(int) time.Duration {
+	return func(tries int) time.Duration {
+		b := asyncevents.DelayExponentialBinary(tries)
+		if b > cap {
+			return cap
+		}
+		return b
+	}
+}
+
+// TODO: implement me!!
+type AlertsEventsConsumer struct {
+	Consumer asyncevents.SaramaMessageConsumer
+}
+
+func (c *AlertsEventsConsumer) Consume(ctx context.Context,
+	session sarama.ConsumerGroupSession, message *sarama.ConsumerMessage) error {
+	err := c.Consumer.Consume(ctx, session, message)
+	if err != nil {
+		session.MarkMessage(message, fmt.Sprintf("I have given up after error: %s", err))
+		return err
 	}
 	return nil
 }

data/events/events_test.go
@@ -1,0 +1,163 @@
+package events
+
+import (
+	"context"
+	"log/slog"
+	"sync"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/tidepool-org/platform/log"
+	"github.com/tidepool-org/platform/log/test"
+)
+
+var _ = Describe("SaramaRunner", func() {
+	Context("has a lifecycle", func() {
+		newTestRunner := func() *SaramaRunner {
+			return &SaramaRunner{
+				Config:       SaramaRunnerConfig{},
+				EventsRunner: &mockEventsRunner{},
+			}
+		}
+		It("starts with Run() and stops with Terminate()", func() {
+			r := newTestRunner()
+			var runErr error
+			var errMu sync.Mutex
+			launched := make(chan struct{}, 1)
+			go func() {
+				errMu.Lock()
+				func() {
+					defer errMu.Unlock()
+					runErr = r.Run()
+					launched <- struct{}{}
+				}()
+			}()
+			<-launched
+			time.Sleep(time.Millisecond)
+			errMu.Lock()
+			defer errMu.Unlock()
+
+			Expect(r.Terminate()).To(Succeed())
+			Eventually(runErr).WithTimeout(10 * time.Millisecond).Should(Succeed())
+		})
+
+		Describe("Run()", func() {
+			var errMu sync.Mutex
+
+			It("can be started only once", func() {
+				r := newTestRunner()
+				var firstRunErr, secondRunErr error
+				launched := make(chan struct{}, 2)
+				go func() {
+					errMu.Lock()
+					func() {
+						defer errMu.Unlock()
+						firstRunErr = r.Run()
+						launched <- struct{}{}
+					}()
+				}()
+				go func() {
+					errMu.Lock()
+					func() {
+						defer errMu.Unlock()
+						secondRunErr = r.Run()
+						launched <- struct{}{}
+					}()
+
+				}()
+				<-launched
+				<-launched
+				errMu.Lock()
+				defer errMu.Unlock()
+
+				// The above doesn't _guarantee_ that Run has been called twice,
+				// but... it should work.
+
+				Expect(r.Terminate()).To(Succeed())
+				if firstRunErr != nil {
+					Expect(firstRunErr).To(MatchError(ContainSubstring("it's already initialized")))
+					Expect(secondRunErr).To(Succeed())
+				} else {
+					Expect(firstRunErr).To(Succeed())
+					Expect(secondRunErr).To(MatchError(ContainSubstring("it's already initialized")))
+				}
+			})
+
+			It("can't be Terminate()'d before it's Run()", func() {
+				r := newTestRunner()
+				Expect(r.Terminate()).To(MatchError(ContainSubstring("it's not running")))
+			})
+		})
+	})
+
+	Describe("logger", func() {
+		It("prefers a context's logger", func() {
+			testLogger := test.NewLogger()
+			ctxLogger := test.NewLogger()
+			r := &SaramaRunner{
+				Config: SaramaRunnerConfig{Logger: testLogger},
+			}
+
+			ctx := log.NewContextWithLogger(context.Background(), ctxLogger)
+			got := r.logger(ctx)
+
+			goCommonLogger, ok := got.(*log.GoCommonAdapter)
+			Expect(ok).To(BeTrue())
+			Expect(goCommonLogger.Logger).To(Equal(ctxLogger))
+		})
+
+		Context("without a context logger", func() {
+			It("uses the configured logger", func() {
+				testLogger := test.NewLogger()
+				r := &SaramaRunner{
+					Config: SaramaRunnerConfig{
+						Logger: testLogger,
+					},
+				}
+
+				got := r.logger(context.Background())
+
+				goCommonLogger, ok := got.(*log.GoCommonAdapter)
+				Expect(ok).To(BeTrue())
+				Expect(goCommonLogger.Logger).To(Equal(testLogger))
+			})
+
+			Context("or any configured logger", func() {
+				It("doesn't panic", func() {
+					r := &SaramaRunner{Config: SaramaRunnerConfig{}}
+					ctx := context.Background()
+					got := r.logger(ctx)
+
+					Expect(func() {
+						got.Log(ctx, slog.LevelInfo, "testing")
+					}).ToNot(Panic())
+				})
+			})
+		})
+	})
+
+	DescribeTable("CappedExponentialBinaryDelay",
+		func(cap time.Duration, input int, output time.Duration) {
+			f := CappedExponentialBinaryDelay(cap)
+			Expect(f(input)).To(Equal(output))
+		},
+		Entry("cap: 1m; tries: 0", time.Minute, 0, time.Second),
+		Entry("cap: 1m; tries: 1", time.Minute, 1, 2*time.Second),
+		Entry("cap: 1m; tries: 2", time.Minute, 2, 4*time.Second),
+		Entry("cap: 1m; tries: 3", time.Minute, 3, 8*time.Second),
+		Entry("cap: 1m; tries: 4", time.Minute, 4, 16*time.Second),
+		Entry("cap: 1m; tries: 5", time.Minute, 5, 32*time.Second),
+		Entry("cap: 1m; tries: 6", time.Minute, 6, time.Minute),
+		Entry("cap: 1m; tries: 20", time.Minute, 20, time.Minute),
+	)
+})
+
+type mockEventsRunner struct {
+	Err error
+}
+
+func (r *mockEventsRunner) Run(ctx context.Context) error {
+	return r.Err
+}